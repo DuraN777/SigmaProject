cc368dfeaa585eccfe8ce2c3cea621d739682b6b
Djura Neskovic <neskovic991@gmail.com>
2024-01-04 21:18:32 +0100

    sync header transitions on about pages

--- src/js/about.js
+++ src/js/about.js
@@ -6,22 +6,36 @@ const navItems = document.querySelectorAll(".nav__item");
 const navLink = document.querySelectorAll(".nav-link ");
 const hamburger = document.querySelector('.header__hamburger');
 
+let lastScrollPosition = 0;
+
 window.onscroll = function(){
   let top = window.scrollY;
 
   if(top >= 100) {
+    header.classList.add("active");
+  }else {
+    header.classList.remove("active");
+  }
+
+  let currentScrollPosition = window.scrollY;
+
+  if(currentScrollPosition > lastScrollPosition) {
     headerLogo.classList.add("active");
+    nav.classList.add("active");
     navSubmenu.classList.add("active");
     navItems.forEach(item => {
       item.classList.add("active");
     })
   }else {
     headerLogo.classList.remove("active");
+    nav.classList.remove("active");
     navSubmenu.classList.remove("active");
     navItems.forEach(item => {
       item.classList.remove("active");
     })
   }
+
+  lastScrollPosition = currentScrollPosition;
 }
 
 // Toggle mobile nav menu
